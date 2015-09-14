20dc3d3e4ebb15ccd75312e28cd13ef07ceac24c
lgrapenthin <grapenthin.leon@gmail.com>
2015-09-14 20:36:46 +0200

    upgrade deps, use figwheel fileserver for dev

--- project.clj
+++ project.clj
@@ -4,14 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cljsjs/react "0.13.3-0"]]
+                 [cljsjs/react "0.13.3-1"]]
   :profiles
   {:dev
-   {:dependencies [[org.clojure/clojurescript "1.7.28"]
+   {:dependencies [[org.clojure/clojurescript "1.7.122"]
                    
                    [sablono "0.3.6"]]
-    :plugins [[lein-cljsbuild "1.0.6"]
-              [lein-figwheel "0.3.7"]]
+    :plugins [[lein-cljsbuild "1.1.0"]
+              [lein-figwheel "0.3.9"]]
+    :clean-targets [:target-path "out" "resources/public/cljs"]
     
     :figwheel {:nrepl-port 7888}
     
@@ -20,4 +21,7 @@
      [{:id "dev"
        :source-paths ["src" "dev"]
        :figwheel true
-       :compiler {:main dev.core}}]}}})
+       :compiler {:main dev.core
+                  :asset-path "cljs/out"
+                  :output-to  "resources/public/cljs/main.js"
+                  :output-dir "resources/public/cljs/out"}}]}}})
