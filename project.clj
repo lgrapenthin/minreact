b93a39a231aade93e874c75bfe7802146c0efce0
Leon Grapenthin <grapenthin.leon@gmail.com>
2018-03-16 19:34:02 +0100

    alpha3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject minreact "0.1.7-alpha2"
+(defproject minreact "0.1.7-alpha3"
   :description "ClojureScript React adapter"
   :url "http://github.com/lgrapenthin/minreact"
   :license {:name "Eclipse Public License"
